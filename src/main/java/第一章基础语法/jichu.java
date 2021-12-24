3878835b4adc54d05513cdd3a677cf5b2a08ba46
jiayixing <1102242300@qq.com>
2021-12-24 22:54:27 +0800

    这是12.24号晚上的第一次提交。

---
+++ src/main/java/第一章基础语法/jichu.java
@@ -0,0 +1,24 @@
+package 第一章基础语法;
+
+/**
+ * @Author XINGXING jia
+ * @Date 2021/12/24 22:14
+ * @Version 1.0
+ */
+public class jichu {
+    public static void main(String[] args) {
+        /**
+         * 1、命名规范
+         * 包名：全部小写   xxyyzz
+         * 类名、接口名：大驼峰 XxYxZz
+         * 变量、方法名：小驼峰 xxYyZZ
+         * 常量：全部大写，下划线分割
+         */
+
+        //基本数据类型    对应 1 2 4 8字节
+        String d = "";
+        System.out.println(d.length());
+        //三元运算符
+        System.out.println(4>2?4:3);
+    }
+}
